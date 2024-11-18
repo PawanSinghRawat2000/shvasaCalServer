168372a4d2672b4c8d36f3350e279c04154ae9d5
PawanSinghRawat2000 <pawansinghrawatis@gmail.com>
2024-11-19 03:54:31 +0530

    fix

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`mongodb://localhost:27017/Shvasa`);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error.message);
