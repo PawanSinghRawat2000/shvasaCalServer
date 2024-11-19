967bcedcb54c2aebc0a9ac183e89e53205924aee
PawanSinghRawat2000 <pawansinghrawatis@gmail.com>
2024-11-19 05:55:01 +0530

    fix

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -37,7 +37,7 @@ exports.emailLogin = async (req, res) => {
             maxAge: 1000 * 60 * 60 * 24 * 10,
             withCredentials: true,
             httpOnly: false,
-            secure: true,
+            secure: process.env.NODE_ENV === 'production',
             sameSite: "none",
         });
         return res.status(200).json({ user: user, message: "Logged in" });
@@ -81,7 +81,7 @@ exports.emailSignup = async (req, res) => {
             maxAge: 1000 * 60 * 60 * 24 * 10,
             withCredentials: true,
             httpOnly: false,
-            secure: true,
+            secure: process.env.NODE_ENV === 'production',
             sameSite: "none",
         });
         return res.status(201).json({
@@ -103,7 +103,7 @@ exports.logout = async (req, res) => {
             maxAge: 0,
             withCredentials: true,
             httpOnly: false,
-            secure: true,
+            secure: process.env.NODE_ENV === 'production',
             sameSite: "none",
         });
 
